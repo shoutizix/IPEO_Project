8b9927c2aefa5bfeff09a4273f0e43d80c70ccc6
Noah Kaltenrieder <noah.kaltenrieder@epfl.ch>
2023-01-11 12:06:57 +0100

    updated all notebooks

--- src/dataloader.py
+++ src/dataloader.py
@@ -28,16 +28,29 @@ class LandCoverData(Dataset):
     
     colormap = ListedColormap(colormap_names)
     
+    mean=torch.tensor([0.43315597, 0.48058005, 0.42299773])
+    std=torch.tensor([0.23198403, 0.22022461, 0.19084829])
+    #std=torch.tensor([0.13840764, 0.13066204, 0.11168083])
+    
+    normalize = T.Normalize(mean, std)
+    # we invert normalization for plotting later
+    std_inv = 1 / (std + 1e-7)
+    unnormalize = T.Normalize(-mean * std_inv, std_inv)
+    
+    input_transform_norm =  T.Compose([
+        T.Resize((200, 200)),
+        T.ToTensor(),
+        normalize])
+    
     input_transform =  T.Compose([
         T.Resize((200, 200)),
         T.ToTensor()])
 
     label_transform = T.ToTensor()
 
-    def __init__(self, path, transforms=None, split='train'):
+    def __init__(self, path, transforms=input_transform_norm, split='train', ignore_last_number=0, use_augmented=False):
         if transforms is not None:
             self.input_transform=transforms
-
 
         self.data = []     
 
@@ -53,6 +66,22 @@ class LandCoverData(Dataset):
                     imgNameRGB,
                     imgNameLabel
                 ))
+            if ignore_last_number != 0:
+                self.data = self.data[:-ignore_last_number]
+        if use_augmented:
+            path_augmented_rgb = f"{path}ipeo_data/augmented_data_rgb/"
+            path_augmented_label = f"{path}ipeo_data/augmented_data_label/"
+            
+            name_list = os.listdir(path_augmented_rgb)
+            for name in name_list:
+                name_without_extension = name[:-4]
+                imgNameRGB = path_augmented_rgb+name_without_extension+".tif"
+                imgNameLabel = path_augmented_label+name_without_extension[:-3]+"label.tif"
+                self.data.append((
+                    imgNameRGB,
+                    imgNameLabel
+                ))  
+            
 
     def __len__(self):
         return len(self.data)
